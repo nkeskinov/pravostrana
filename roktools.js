912be3cbb60ade633bef1765ecbdb015b57034fd
Dragan Milchevski <dragan.milcevski@gmail.com>
2009-05-02 20:29:41 +0000

    prv komit

---
+++ roktools.js
@@ -0,0 +1,8 @@
+/* variables for top panel */
+var panelSize = 120;
+var effectDuration = 750;
+var openString = "Open Control Panel";
+var closeString = "Close Control Panel";
+var transition = Fx.Transitions.circOut;
+
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('w.j(9(){c()});c=9(){$(\'2\').4=$(\'2\').v(\'r\',{p:m,b:b});$(\'2\').4.i(-5);$(\'2\').3=0;$(\'u\').d.q=5+\'o\';$(\'n\').d.l="k";$(\'6\').7.8=a;$(\'h\').g(\'f\',9(){s($(\'2\').3==1){$(\'2\').4.e(0,-5);$(\'2\').3=0;$(\'6\').7.8=a}t{$(\'2\').4.e(-5,0);$(\'2\').3=1;$(\'6\').7.8=x}})};',34,34,'||fxContainer|state|fx|panelSize|fxTrigger|lastChild|nodeValue|function|openString|transition|initPullDown|style|start|click|addEvent|fxTab|set|onDomReady|visible|visibility|effectDuration|fxPadding|px|duration|height|top|if|else|fxTarget|effect|window|closeString'.split('|'),0,{}))
