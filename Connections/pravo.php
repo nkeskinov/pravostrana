c05d792fd7a5fd0a2d29d02dc660a9e0f96e7ebc
nkeskinov <nkeskinov@gmail.com>
2009-05-12 22:18:08 +0000

    Dodavanje na uste edna (komentirana) linija kod za kako da se pusti UTF-8 poddrska za MySQL koga nejzinata verzija e pogolema od 5.0.7. Za zal, verzijata koja ja imame na raspolaganje e 5.0.67

--- Connections/pravo.php
+++ Connections/pravo.php
@@ -7,6 +7,8 @@ $database_pravo = "pravodb";
 $username_pravo = "pravodb";
 $password_pravo = "Pravoorgmk1";
 $pravo = mysql_pconnect($hostname_pravo, $username_pravo, $password_pravo) or trigger_error(mysql_error(),E_USER_ERROR); 
-# Enable UTF-8 transactions in MySQL
+# Enable UTF-8 transactions in MySQL < 5.0.7
 mysql_query('set NAMES utf8'); 
+# Enable UTF-8 transactions in MySQL >= 5.0.7
+# mysql_set_charset('utf8');
 ?>
