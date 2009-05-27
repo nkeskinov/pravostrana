e6fc9608fd8e6831a62e577c32ea423495821c06
dragan.milcevski <dragan.milcevski@ffcaf4a2-3745-11de-96ab-f16d9912eac9>
2009-05-27 21:41:46 +0000

    session_start() vo register.php

--- trunk/register.php
+++ trunk/register.php
@@ -1,3 +1,7 @@
+<?php 
+if (!isset($_SESSION)) {
+session_start();
+} ?>
 <?php require_once("Connections/pravo.php"); ?>
 <?php include("util/misc.php"); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
