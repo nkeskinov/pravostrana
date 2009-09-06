b6267025fe834989c77f00e73250a6287e2986ca
Dragan Milchevski <dragan.milcevski@gmail.com>
2009-09-06 21:10:37 +0000

    МНогу ситни измени

--- news.php
+++ news.php
@@ -24,77 +24,29 @@ if(isset($_SESSION['MM_ID']))
 trackVisit($ip_address, $referrer, $browser, $language, $id_user, $page, $from_page, $database_pravo, $pravo);
 ?>
 
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/CleanTemplate.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/SingleRed.dwt.php" codeOutsideHTMLIsLocked="false" -->
 <head>
 <!--templateinfo codeoutsidehtmlislocked="true" -->
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link href="rokmoomenu.css" rel="stylesheet" type="text/css" />
-<link href="style.css" rel="stylesheet" type="text/css" />
-<link rel="shortcut icon" href="images/favicon1.png" />
+<link href="/pravo.org.mk/rokmoomenu.css" rel="stylesheet" type="text/css" />
+<link href="/pravo.org.mk/style.css" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" href="/pravo.org.mk/images/favicon1.png" />
 <!-- InstanceBeginEditable name="doctitle" -->
-<title>Pravo.org.mk | Новости</title>
+<title>Pravo.org.mk | Новостиk</title>
 <!-- InstanceEndEditable -->
 <link href="YUI/2.6.0/build/fonts/fonts-min.css" rel="stylesheet" type="text/css" />
 <link href="YUI/2.6.0/build/container/assets/skins/sam/container.css" rel="stylesheet" type="text/css" />
 <script src="YUI/2.6.0/build/yahoo-dom-event/yahoo-dom-event.js" type="text/javascript"></script>
 <script src="YUI/2.6.0/build/container/container-min.js" type="text/javascript"></script>
 
-<script type="text/javascript" src="roktools.js"></script>
+<script type="text/javascript" src="/pravo.org.mk/roktools.js"></script>
 
-<script type="text/javascript" src="mootools.js"></script>
+<script type="text/javascript" src="/pravo.org.mk/javaScripts/cirillic_converter.js"></script>
+<script type="text/javascript" src="/pravo.org.mk/mootools.js"></script>
 
-<script type="text/javascript" src="javaScripts/cirillic_converter.js"></script>
-<script type="text/javascript" src="rokmoomenu.js"></script>
-<script language="javascript" type="text/javascript" src="javaScripts/prototype.js"></script><script language="javascript" type="text/javascript" src="javaScripts/autoExpandContract.js"></script>
- <script type="text/javascript">  
-         // <![CDATA[  
-         document.observe('dom:loaded', function() {  
-             $$('.highlight').each(function(item) {  
-                 item.observe('focus', function(){   
-                     item.style.backgroundColor = "FDFFDE";  
-                 });  
-                 item.observe('blur', function(){   
-                     item.style.backgroundColor = "ffffff";  
-                 });               
-             });  
-             
-         });  
-         //   
-         </script> 
-<script type="text/javascript">
-<!--
-function MM_jumpMenu(targ,selObj,restore){ //v3.0
-  eval(targ+".location='"+selObj.options[selObj.selectedIndex].value+"'");
-  if (restore) selObj.selectedIndex=0;
-}
-//-->
-</script>
-<script type="text/javascript">
-<!--
-function MM_swapImgRestore() { //v3.0
-  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
-}
-function MM_preloadImages() { //v3.0
-  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
-    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
-    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
-}
-
-function MM_findObj(n, d) { //v4.01
-  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
-    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
-  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
-  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
-  if(!x && d.getElementById) x=d.getElementById(n); return x;
-}
-
-function MM_swapImage() { //v3.0
-  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
-   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
-}
-//-->
-</script>
+<script type="text/javascript" src="/pravo.org.mk/rokmoomenu.js"></script>
 <script type="text/javascript">
 
 window.addEvent('domready', function() {
@@ -127,11 +79,6 @@ transition: Fx.Transitions.sineOut
 
 </script>
 
-<style type="text/css">
-<!--
-
--->
-</style>
 <!-- InstanceBeginEditable name="head" -->
 <!-- InstanceEndEditable -->
 </head>
@@ -141,18 +88,19 @@ transition: Fx.Transitions.sineOut
 <div id="wrapper">
 	<div class="header">
    	  <div class="header-top">
-   	    <div id="logoText"><img src="images/banner.png" /></div>
+   	    <div id="logoText"><img src="/pravo.org.mk/images/banner.png" /></div>
+        <div id="bannerDiv1"><?php getBanner($database_pravo, $pravo, 2); ?></div>
    	  </div>
       <div id="horiz-menu" class="moomenu"><!-- InstanceBeginEditable name="Menu" -->
-        <ul class="nav">
+          <ul class="nav">
           <li><a href="index.php">Почетна</a></li>
           <li><a class="topdaddy" href="documentlaws.php">Закони</a></li>
           <li><a href="analysis.php">Анализи</a></li>
           <li><a href="regulations.php">Прописи</a></li>
-          <li><a href="courtpractice.php">Судска Пракса</a>
+          <li><a href="#">Судска пракса</a>
            <ul>
-             <li><a href="courtpractice.php">Судска Пракса</a></li>
-             <li><a href="europeancourt.php">Европски суд за човекови права</a></li>
+            <li><a href="courtpractice.php">Судска пракса</a></li>
+            <li><a href="europeancourt.php">Европски суд за човекови права</a></li>
            </ul>
           </li>
           <li class="active"><a href="news.php">Новости</a></li>
@@ -160,75 +108,39 @@ transition: Fx.Transitions.sineOut
         </ul>
       <!-- InstanceEndEditable -->
         <div id="menu"></div>
-        <div id="mapMenu">
-       <!-- InstanceBeginEditable name="SiteMap" --><table cellpadding="0" cellspacing="0"><tr><td><a href="index.php">Почетна</a>&nbsp;&nbsp;&nbsp;&raquo;&nbsp;</td><td>Новости&nbsp;</td></tr></table><!-- InstanceEndEditable -->
-       </div>
+       <div id="mapMenu"><!-- InstanceBeginEditable name="siteMap" --><table cellpadding="0" cellspacing="0"><tr><td><a href="index.php">Почетна</a>&nbsp;&nbsp;&nbsp;&raquo;&nbsp;</td></tr></table><!-- InstanceEndEditable --></div> 
       </div>
 	</div>
     
     <div class="mainBody">
    	  <div class="content">
             <div><?php getBanner($database_pravo, $pravo, 1); ?></div>
-            <!-- InstanceBeginEditable name="Content" -->
-            <table width="100%">
-              <tr>
-                <td width="28%" valign="top"><div class="left-block">
-                  <div class="title">
+            <table><tr><td>
+            <div class="middle-red-block">
+                <div class="title">
                     <div class="left"></div>
-                    <div class="middle">
-                      <div class="text">Архива</div>
-                    </div>
+                    <div class="middle"><div class="text"><!-- InstanceBeginEditable name="BlockTitle" -->Новости<!-- InstanceEndEditable --></div></div>
                     <div class="right"></div>
-                  </div>
-                  <div class="sodrzina">
-                    <p>Текст </p>
-                  </div>
-                </div></td>
-                <td width="72%" valign="top"><div class="right-block-bigger">
-                  <div class="title">
-                    <div class="left"></div>
-                    <div class="middle">
-                      <div class="text">Новости</div>
-                    </div>
-                    <div class="right"></div>
-                  </div>
-                  <div class="sodrzina">
-					<?php include("util/news.php"); ?>
-                  </div>
-                </div></td>
-              </tr>
-              <tr>
-                <td>&nbsp;</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td valign="top"></td>
-                <td valign="top"></td>
-              </tr>
-            </table>
-            <!-- InstanceEndEditable -->
-        <p>&nbsp;</p>
-            <p>&nbsp;</p>
-            <p>&nbsp;</p>
-
+                </div>
+                 <div class="sodrzina"><!-- InstanceBeginEditable name="BlockContent" -->
+                   <?php include("util/news.php"); ?>
+                 <!-- InstanceEndEditable --></div>
+            </div>
+            </td></tr></table>
+   		 <p>&nbsp;</p>   
       </div>
-
- 
         <div class="right">
-        <?php include("util/login_block.php"); ?>
-          <div style="width:250px; margin-top:5px; margin-bottom:5px;"><!-- InstanceBeginEditable name="SearchRegion" --><!-- InstanceEndEditable --></div>
-         &nbsp;
-          <div>
-            <div><?php getBanner($database_pravo, $pravo, 3); ?></div>
-          	<br />
-          	<div><?php getBanner($database_pravo, $pravo, 4); ?></div>
-          	<br />
-          	<div><?php getBanner($database_pravo, $pravo, 5); ?></div>          
-          </div>
-          </div>
+            <?php include("util/login_block.php"); ?>
+               <br />
+              <div><?php getBanner($database_pravo, $pravo, 3); ?></div>
+              <br />
+              <div><?php getBanner($database_pravo, $pravo, 4); ?></div>
+              <br />
+              <div><?php getBanner($database_pravo, $pravo, 5); ?></div>
+              </div>
 
     </div>
-    
+    <div class="above-footer"></div>
 	<div class="footer">Copyright &copy; 2009 Сите права задржани</div>	
     <div style="margin-top:-30px; color:#999; float:left; width:100%;">Pravo.org.mk не презема одговорност за евентуалните грешки во текстот на законите <div style="float:right;"><a href="http://camost.org" target="_blank"><img src="images/most.jpg" border="0"/></a></div></div>
 </div>
