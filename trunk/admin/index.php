98b2c451b73c5fe4a4934085a2b87eb9d42d1a5f
nkeskinov <nkeskinov@ffcaf4a2-3745-11de-96ab-f16d9912eac9>
2009-09-12 15:07:32 +0000

    Sredeni linkanja od /admin/ kon obicniot del

--- trunk/admin/index.php
+++ trunk/admin/index.php
@@ -69,18 +69,18 @@ transition: Fx.Transitions.sineOut
    	  </div>
       <div id="horiz-menu" class="moomenu"><!-- InstanceBeginEditable name="Menu" -->
         <ul class="nav">
-           <li><a href="/pravo.org.mk/index.php">Почетна</a></li>
-          <li class="active"><a class="topdaddy" href="/pravo.org.mk/documentlaws.php">Закони</a></li>
-         <li><a href="/pravo.org.mk/analysis.php">Анализи</a></li>
-          <li><a href="/pravo.org.mk/regulations.php">Прописи</a></li>
-          <li><a href="courtpractice.php">Судска пракса</a>
+           <li><a href="../index.php">Почетна</a></li>
+          <li class="active"><a class="topdaddy" href="../documentlaws.php">Закони</a></li>
+         <li><a href="../analysis.php">Анализи</a></li>
+          <li><a href="../regulations.php">Прописи</a></li>
+          <li><a href="../courtpractice.php">Судска пракса</a>
            <ul>
-             <li><a href="/pravo.org.mk/courtpractice.php">Судска пракса</a></li>
-            <li><a href="/pravo.org.mk/europeancourt.php">Европски суд за човекови права</a></li>
+             <li><a href="../courtpractice.php">Судска пракса</a></li>
+            <li><a href="../europeancourt.php">Европски суд за човекови права</a></li>
            </ul>
           </li>
-          <li><a href="/pravo.org.mk/news.php">Новости</a></li>
-          <li><a href="/pravo.org.mk/contact.php">Контакт</a></li>
+          <li><a href="../news.php">Новости</a></li>
+          <li><a href="../contact.php">Контакт</a></li>
         </ul>
       <!-- InstanceEndEditable -->
        <div id="mapMenu"><!-- InstanceBeginEditable name="SiteMap" --><table cellpadding="0" cellspacing="0"><tr><td><a href="index.php">Почетна</a>&nbsp;&nbsp;&nbsp;&raquo;&nbsp;</td></tr></table><!-- InstanceEndEditable --></div> 
