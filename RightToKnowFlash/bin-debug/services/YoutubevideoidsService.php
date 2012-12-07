1755110dfb4307541fe2b841475d16426fe9a4e9
nkeskinov@gmail.com <nkeskinov@gmail.com@ffcaf4a2-3745-11de-96ab-f16d9912eac9>
2012-12-07 03:38:24 +0000

    Updated translations Added youtube service from DB. Added youtube for help window. and stuff

---
+++ RightToKnowFlash/bin-debug/services/YoutubevideoidsService.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ *  README for sample service
+ *
+ *  This generated sample service contains functions that illustrate typical service operations.
+ *  Use these functions as a starting point for creating your own service implementation. Modify the 
+ *  function signatures, references to the database, and implementation according to your needs. 
+ *  Delete the functions that you do not use.
+ *
+ *  Save your changes and return to Flash Builder. In Flash Builder Data/Services View, refresh 
+ *  the service. Then drag service operations onto user interface components in Design View. For 
+ *  example, drag the getAllItems() operation onto a DataGrid.
+ *  
+ *  This code is for prototyping only.
+ *  
+ *  Authenticate the user prior to allowing them to call these methods. You can find more 
+ *  information at http://www.adobe.com/go/flex_security
+ *
+ */
+class YoutubevideoidsService {
+
+	var $username = "righttoknowdb";
+	var $password = "rightoKnow2012";
+	var $server = "localhost";
+	var $port = "3306";
+	var $databasename = "righttoknowdb";
+	var $tablename = "youtube_video_ids";
+
+	var $connection;
+
+	/**
+	 * The constructor initializes the connection to database. Everytime a request is 
+	 * received by Zend AMF, an instance of the service class is created and then the
+	 * requested method is invoked.
+	 */
+	public function __construct() {
+	  	$this->connection = mysqli_connect(
+	  							$this->server,  
+	  							$this->username,  
+	  							$this->password, 
+	  							$this->databasename,
+	  							$this->port
+	  						);
+
+		$this->throwExceptionOnError($this->connection);
+	}
+
+	/**
+	 * Returns the item corresponding to the value specified for the primary key.
+	 *
+	 * Add authorization or any logical checks for secure access to your data 
+	 *
+	 * 
+	 * @return stdClass
+	 */
+	public function getYoutube_video_idsByID($itemID) {
+		
+		$stmt = mysqli_prepare($this->connection, "SELECT * FROM $this->tablename where id_youtube_video_id=?");
+		$this->throwExceptionOnError();
+		
+		mysqli_stmt_bind_param($stmt, 'i', $itemID);		
+		$this->throwExceptionOnError();
+		
+		mysqli_stmt_execute($stmt);
+		$this->throwExceptionOnError();
+		
+		mysqli_stmt_bind_result($stmt, $row->id_youtube_video_id, $row->url_id, $row->url_id_sq, $row->url_id_en);
+		
+		if(mysqli_stmt_fetch($stmt)) {
+	      return $row;
+		} else {
+	      return null;
+		}
+	}	
+	
+	/**
+	 * Utility function to throw an exception if an error occurs 
+	 * while running a mysql command.
+	 */
+	private function throwExceptionOnError($link = null) {
+		if($link == null) {
+			$link = $this->connection;
+		}
+		if(mysqli_error($link)) {
+			$msg = mysqli_errno($link) . ": " . mysqli_error($link);
+			throw new Exception('MySQL Error - '. $msg);
+		}		
+	}
+}
+
+?>
